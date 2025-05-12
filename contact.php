2c83a83a34cf6fe12f75d7eac655218762b2205a
Carlos <carlos.pellit@gmail.com>
2025-05-12 13:17:12 +0200

    actualizacion e-mail contacto

--- contact.php
+++ contact.php
@@ -9,39 +9,14 @@ function isEmail($email_contact ) {
 
 if (!defined("PHP_EOL")) define("PHP_EOL", "\r\n");
 
-$name_contact     = $_POST['name_contact'];
-$lastname_contact    = $_POST['lastname_contact'];
-$email_contact    = $_POST['email_contact'];
-$phone_contact   = $_POST['phone_contact'];
-$message_contact = $_POST['message_contact'];
-$verify_contact   = $_POST['verify_contact'];
 
-if(trim($name_contact) == '') {
-	echo '<div class="error_message">Debes introducir un nombre.</div>';
-	exit();
-} else if(trim($lastname_contact ) == '') {
-	echo '<div class="error_message">Debes introducir un apellido.</div>';
-	exit();
-} else if(trim($email_contact) == '') {
+$email_contact    = $_POST['email_contact'];
+
+if(trim($email_contact) == '') {
 	echo '<div class="error_message">Por favor, introduce un email válido.</div>';
 	exit();
 } else if(!isEmail($email_contact)) {
 	echo '<div class="error_message">Has introducido un email no válido, por favor inténtalo otra vez.</div>';
-	exit();
-	} else if(trim($phone_contact) == '') {
-	echo '<div class="error_message">Por favor introduce un teléfono válido.</div>';
-	exit();
-} else if(!is_numeric($phone_contact)) {
-	echo '<div class="error_message">El teléfono solo admite números.</div>';
-	exit();
-} else if(trim($message_contact) == '') {
-	echo '<div class="error_message">Por favor, introduce tu mensaje.</div>';
-	exit();
-} else if(!isset($verify_contact) || trim($verify_contact) == '') {
-	echo '<div class="error_message"> Por favor, introduce el número de verificación.</div>';
-	exit();
-} else if(trim($verify_contact) != '4') {
-	echo '<div class="error_message">El número de verificación es erróneo.</div>';
 	exit();
 }
 
@@ -54,12 +29,12 @@ $address = "info@exemplo.com"; // e-mail a personlizar con el de destino
 
 
 // Below the subject of the email
-$e_subject = 'Has sido contactado por' . $name_contact . '.';
+$e_subject = 'Has sido contactado por' . $email_contact . '.';
 
 // You can change this if you feel that you need to.
-$e_body = "Has sido contactado por $name_contact $lastname_contact con el siguiente mensaje a continuación:" . PHP_EOL . PHP_EOL;
+$e_body = "Has sido contactado por $email_contact  con el siguiente mensaje a continuación:" . PHP_EOL . PHP_EOL;
 $e_content = "\"$message_contact\"" . PHP_EOL . PHP_EOL;
-$e_reply = "Tu puedes contactar $lastname_contact via email, $email_contact o via telefónica: $phone_contact";
+$e_reply = "Tu puedes contactar $email_contact via email, $email_contact ";
 
 $msg = wordwrap( $e_body . $e_content . $e_reply, 70 );
 
@@ -70,8 +45,8 @@ $headers .= "Content-type: text/plain; charset=utf-8" . PHP_EOL;
 $headers .= "Content-Transfer-Encoding: quoted-printable" . PHP_EOL;
 
 $user = "$email_contact";
-$usersubject = "Gracias";
-$userheaders = "From: info@exemplo.com\n";
+$usersubject = "Este no es un sitio para contratar ninguna entrada!";
+$userheaders = "From: carlos.pellit@gmail.com\n";
 $usermessage = "Gracias por contactar conmigo, te responderé lo antes posible.";
 mail($user,$usersubject,$usermessage,$userheaders);
 
@@ -80,7 +55,7 @@ if(mail($address, $e_subject, $msg, $headers)) {
 	// Success message
 	echo "<div id='success_page' style='padding:20px 20px 20px 0'>";
 	echo "<strong >Email enviado.</strong>";
-	echo "Gracias <strong>$name_contact</strong>,<br> tu mensaje ha sido enviado correctamente. Te contactaremos lo antes posible.";
+	echo "Gracias ,<br> tu mensaje ha sido enviado correctamente. Te contactaremos lo antes posible.";
 	echo "</div>";
 
 } else {
